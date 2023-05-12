fd12156ad68081911fa2c6622cf4b60273a8607f
Vishal123456-git <vambolikar.ibl@gmail.com>
2023-05-11 23:27:37 -0700

    Add existing project files to Git

---
+++ js/custome.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(".toggle-bar").click(function(){
+     $("nav ul").slideToggle();
+    });
+  });
+
+
+  // FAQ
+  $(document).ready(function(){
+    $(".accordion").on("click", ".heading", function() {
+
+    $(this).toggleClass("active").next().slideToggle();
+
+    $(".contents").not($(this).next()).slideUp(300);
+                 
+    $(this).siblings().removeClass("active");
+    });
+   });
+        
